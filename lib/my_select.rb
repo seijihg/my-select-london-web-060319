e39c88baf8daf632bd3fd7ef18d35796120afcdf
Le H Ngo <seiji@o2.pl>
2019-05-22 14:45:26 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ array_n = []
+
+ while i < collection.length
+   if collection[i] == yield[i]
+     array_n.push(yield[i])
+   end
+   i += 1
+ end
+array_n
 end
